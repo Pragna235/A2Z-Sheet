1814f6a2755041193163abd482091062ff2a7d86
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-11-26 12:16:35 +0530

    Create Switch Case Statement.cpp

---
+++ Switch Case Statement.cpp
@@ -0,0 +1,14 @@
+
+double areaSwitchCase(int ch, vector<double> a) {
+	// Write your code here
+	switch(ch){
+		case 1:
+		return ( 3.14159265358979323846 * a[0] * a[0]);
+		break;
+
+		case 2:
+		return (a[0]* a[1]);
+		break;
+	}
+
+}
